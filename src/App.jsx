ac193aad588d9cf301089e35790c47665ead48da
Udhaya007Kumar <udhaya93kumar@gmail.com>
2025-01-16 16:55:19 +0530

    dardmiode change

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import DataSourcemagement from "./Pages/DataSourcemagement";
 function App() {
   return (
     <div>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
         <BrowserRouter>
            {/* <Page />  */}
           <Routes>
