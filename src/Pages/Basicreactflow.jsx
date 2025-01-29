9817a4361738f943a5cfe4f7c022066566946d87
Udhaya007Kumar <udhaya93kumar@gmail.com>
2025-01-29 18:10:55 +0530

    fixed exxro

--- src/Pages/Basicreactflow.jsx
+++ src/Pages/Basicreactflow.jsx
@@ -7,7 +7,8 @@ import {
   Background,
 } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
-import { DatabaseSchemaNode } from "@/components/database-schema-node";
+import { DatabaseSchemaNode } from "../components/database-schema-node";
+
 
 const initialNodes = [
   {
